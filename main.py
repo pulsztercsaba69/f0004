019d8c3daff44826797b2911600ed5827eb66139
pulsztercsaba <pulsztercsaba7@gmail.com>
2020-11-19 08:44:28 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+#mod1
+vezetéknév = input('mi a vezetéknév?')
+keresztnév = input('mi a keresztnév?')
+print(f'A te neved {vezetéknév} {keresztnév}.')
+
+#mod2
+vezetéknév = input('mi a vezetéknév?')
+keresztnév = input('mi a keresztnév?')
+print('A te neved', vezetéknév, keresztnév, '.', sep='')
+
+#mod3
+nev1 = input('mi a vezetéknév?')
+nev2 = input('mi a keresztnév?')
+print('A te neved', nev1, nev2 + '.')
